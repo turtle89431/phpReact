6d48feadd44239fac9d50804af20b3a13dc66f85
turtle89431 <turtle89431@users.noreply.github.com>
2018-02-28 13:20:18 -0800

    fix

--- test.php
+++ test.php
@@ -8,8 +8,8 @@ $sec=(isset($_GET['s']))?'s':'';
 $sel=(isset($_GET['sel']))?$_GET['sel']:'';
 $path=(isset($_GET['path']))?$_GET['path']:'';
 //$uri= sprintf("http%s://%s/%s",$sec,$url,$path);
-$uri=sprintf("https://www.dice.com/jobs?q=Information+Technology&l=95965")
+$uri=sprintf("https://www.dice.com/jobs?q=Information+Technology&l=95965");
 $test=file_get_html($uri);
-$out = $test->find($sel);
+$out = $test->find('#div.serp-result-content.bold-highlight');
 echo json_encode($out)
 ?>
