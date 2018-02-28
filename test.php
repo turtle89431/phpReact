1349cc56d2232f0ab793452b72d364b3c2c25745
turtle89431 <turtle89431@users.noreply.github.com>
2018-02-28 13:24:16 -0800

    tc block

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 <?php 
 include_once("./grab.php");
-
+try{
 $url=(isset($_GET['url']))?$_GET['url']:'';
 $sec=(isset($_GET['s']))?'s':'';
 $sel=(isset($_GET['sel']))?$_GET['sel']:'';
@@ -11,5 +11,7 @@ $path=(isset($_GET['path']))?$_GET['path']:'';
 $uri=sprintf("https://www.dice.com/jobs?q=Information+Technology&l=95965");
 $test=file_get_html($uri);
 $out = $test->find('#div.serp-result-content.bold-highlight');
-echo json_encode($out)
+echo json_encode($out)}catch(Exception $e) {
+    echo($e->getMessage());
+}
 ?>
