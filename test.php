df27f28a15b4caa34922b3052adda0cdaaeb9ff2
turtle89431 <turtle89431@users.noreply.github.com>
2018-02-28 14:28:12 -0800

    include

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-require_once('Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php');
+include_once('Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php');
 use Sunra\PhpSimple\HtmlDomParser;
 $dom = HtmlDomParser::file_get_html( $file_name );
 
