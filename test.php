6a157128d5f7e1d7ebeb27566759c71aca9885db
turtle89431 <turtle89431@users.noreply.github.com>
2018-02-28 14:29:07 -0800

    //c

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 include_once('Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php');
-use Sunra\PhpSimple\HtmlDomParser;
+//use Sunra\PhpSimple\HtmlDomParser;
 $dom = HtmlDomParser::file_get_html( $file_name );
 
 $elems = $dom->find($elem_name);
